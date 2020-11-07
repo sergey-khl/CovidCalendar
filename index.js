03a5cdd7912308d04ffc8d02308f65fac1264752
sergey-khl <64624048+sergey-khl@users.noreply.github.com>
2020-11-07 15:31:24 -0700

    add country to dropdown

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
+const countryList = document.querySelector("#country")
+
 var requestOptions = {
-  method: 'GET',
-  redirect: 'follow'
-};
-
-fetch("https://api.covid19api.com/", requestOptions)
-  .then(response => response.json())
-  .then(result => console.log(result["countriesRoute"]))
-  .catch(error => console.log('error', error));
-
-  fetch("https://api.covid19api.com/countries", requestOptions)
-  .then(response => response.json())
-  .then(result => result.forEach(element => {
-      console.log(element);
-  }))
-  .catch(error => console.log('error', error));
+    method: 'GET',
+    redirect: 'follow'
+  };
+  
+  fetch("https://api.covid19api.com/", requestOptions)
+    .then(response => response.json())
+    .then(result => console.log(result["countriesRoute"]))
+    .catch(error => console.log('error', error));
+  
+    fetch("https://api.covid19api.com/countries", requestOptions)
+    .then(response => response.json())
+    .then(result => result.forEach(element => {
+        countryList.innerHTML += "<option>" + element["Country"] + "</option>"
+    }))
+    .catch(error => console.log('error', error));
