8f0d6b0fb1dc2960cd8d514006946eaaef32a794
htellambura <59628363+htellambura@users.noreply.github.com>
2020-11-07 16:01:37 -0700

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,17 @@ var requestOptions = {
     fetch("https://api.covid19api.com/countries", requestOptions)
     .then(response => response.json())
     .then(result => result.forEach(element => {
-        countryList.innerHTML += "<option>" + element["Country"] + "</option>"
+        var country = document.createElement("option")
+        country.innerHTML = element["Country"]
+        country.value = element["Slug"]
+        countryList.appendChild(country)
     }))
     .catch(error => console.log('error', error));
+
+
+var el = document.getElementById('chosendate')
+el.addEventListener('change', () => {
+  const month = el.value
+  var chosenDate = "confirmed?from=" + month + "-01T00:00:00Z&to=" + month + "-30T00:00:00Z"
+  alert(chosenDate)
+})
