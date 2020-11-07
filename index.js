8fd52aa087b5667f74b99e773fd6dcccd1f29d92
htellambura <59628363+htellambura@users.noreply.github.com>
2020-11-07 15:18:10 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-####
+var requestOptions = {
+  method: 'GET',
+  redirect: 'follow'
+};
+
+fetch("https://api.covid19api.com/", requestOptions)
+  .then(response => response.json())
+  .then(result => console.log(result["countriesRoute"]))
+  .catch(error => console.log('error', error));
+
+  fetch("https://api.covid19api.com/countries", requestOptions)
+  .then(response => response.json())
+  .then(result => result.forEach(element => {
+      console.log(element);
+  }))
+  .catch(error => console.log('error', error));
