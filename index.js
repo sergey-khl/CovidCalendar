8cc97a785f560646b034bd530ad6c2de33e69298
sergey-khl <64624048+sergey-khl@users.noreply.github.com>
2020-11-07 16:35:21 -0700

    get value of country and date

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const countryList = document.querySelector("#country")
+const dateSelect = document.getElementById('chosendate')
+const submitButton = document.querySelector(".submit-button")
 
 var requestOptions = {
     method: 'GET',
     redirect: 'follow'
   };
   
-  fetch("https://api.covid19api.com/", requestOptions)
+fetch("https://api.covid19api.com/", requestOptions)
     .then(response => response.json())
     .then(result => console.log(result["countriesRoute"]))
     .catch(error => console.log('error', error));
-  
-    fetch("https://api.covid19api.com/countries", requestOptions)
+
+fetch("https://api.covid19api.com/countries", requestOptions)
     .then(response => response.json())
     .then(result => result.forEach(element => {
         var country = document.createElement("option")
@@ -20,10 +22,19 @@ var requestOptions = {
     }))
     .catch(error => console.log('error', error));
 
-
-var el = document.getElementById('chosendate')
-el.addEventListener('change', () => {
-  const month = el.value
-  var chosenDate = "confirmed?from=" + month + "-01T00:00:00Z&to=" + month + "-30T00:00:00Z"
-  alert(chosenDate)
+submitButton.addEventListener('click', function(){
+    function getSelectedOption(sel) {
+        var opt;
+        for ( var i = 0, len = sel.options.length; i < len; i++ ) {
+            opt = sel.options[i];
+            if ( opt.selected === true ) {
+                break;
+            }
+        }
+        return opt;
+    }
+    var countryName = getSelectedOption(countryList);
+    const month = dateSelect.value;
+    var chosenDate = "confirmed?from=" + month + "-01T00:00:00Z&to=" + month + "-30T00:00:00Z"
+    alert(countryName.value + "\n" + chosenDate)
 })
