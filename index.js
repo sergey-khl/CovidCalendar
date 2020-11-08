d4ebcde3974c17cdfcbd90de7a605622c393f428
sergey-khl <64624048+sergey-khl@users.noreply.github.com>
2020-11-07 19:56:02 -0700

    sort of working cell creator

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const dateSelect = document.getElementById('chosendate')
 const submitButton = document.querySelector(".submit-button")
 let calendarTitle = document.getElementById('calendar-title')
 let youHaveChosen = document.getElementById('you-have-chosen')
-let calendarGrid = document.querySelector('date-grid')
+let calendarGrid = document.querySelector('.date-grid')
 const months = {
   '01':'January',
   '02':'February',
@@ -41,6 +41,7 @@ fetch("https://api.covid19api.com/countries", requestOptions)
 
 
 submitButton.addEventListener('click', function (e) {
+    currday = 1
     e.preventDefault()
     function getSelectedOption(sel) {
         var opt;
@@ -63,18 +64,45 @@ submitButton.addEventListener('click', function (e) {
     calendarTitle.innerHTML = months[yearMonth[1]] + " " + yearMonth[0]
     if (Number(yearMonth[1]) < 12) {
       var nextMonth = ("00"+ (Number(yearMonth[1])+1)).slice(-2)
-      chosenDate = "confirmed?from=" + month + "-01T00:00:00Z&to=" + nextMonth + "-01T00:00:00Z"
+      chosenDate = "confirmed?from=" + month + "-01T00:00:00Z&to=2020-" + nextMonth + "-01T00:00:00Z"
       alert(chosenDate)
     } else {
       chosenDate = "confirmed?from=" + month + "-01T00:00:00Z&to=" + month + "-31T00:00:00Z"
       alert(chosenDate)
     }
-    youHaveChosen.innerHTML = "Confirmed cases in "+countryName.innerHTML+" in "+months[yearMonth[1]];
+    youHaveChosen.innerHTML = "Confirmed cases in " + countryName.innerHTML + " in "+months[yearMonth[1]];
 
+    // reset calendar grid
+    calendarGrid.innerHTML = ""
     fetch("https://api.covid19api.com/country/" + countryName.value + "/status/" + chosenDate, requestOptions)
         .then(response => response.json())
         .then(result => result.forEach(element => {
-            console.log(element["Cases"])
+            // create cell in calendar
+            var cell = document.createElement("button")
+            var day = document.createElement("h3")
+            day.textContent = currday
+            currday += 1
+            day.setAttribute('class', 'date')
+            cell.appendChild(day)
+            div = document.createElement('div')
+            div.setAttribute('class', 'covid-data')
+            var totalCases = document.createElement("h3")
+            totalCases.textContent = "Total Cases: " + element["Cases"]
+            totalCases.setAttribute('class', 'total-cases')
+            var newCases = document.createElement("h3")
+            newCases.textContent = "New Cases: " + element["Cases"]
+            newCases.setAttribute('class', 'new-cases')
+            var change = document.createElement("h3")
+            change.textContent = "% Change: " + element["Cases"]
+            change.setAttribute('class', '%-change')
+            div.appendChild(totalCases)
+            div.appendChild(newCases)
+            div.appendChild(change)
+            cell.appendChild(div)
+
+            // add cell to calendar
+            calendarGrid.appendChild(cell)
+
         }))
         .catch(error => console.log('error', error));
 })
