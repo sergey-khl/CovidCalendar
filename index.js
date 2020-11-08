22d3d885d4e93ce0989c8e25995162fadaf94037
htellambura <59628363+htellambura@users.noreply.github.com>
2020-11-07 17:55:28 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const countryList = document.querySelector("#country")
 const dateSelect = document.getElementById('chosendate')
 const submitButton = document.querySelector(".submit-button")
-var slug
-var date
+let calendarTitle = document.getElementById('calendar-title')
+const months = {
+  '01':'January',
+  '02':'February',
+  '03':'March',
+  '04':'April',
+  '05':'May',
+  '06':'June',
+  '07':'July',
+  '08':'August',
+  '09':'September',
+  '10':'October',
+  '11':'November',
+  '12':'December'
+}
 
 var requestOptions = {
     method: 'GET',
@@ -40,6 +53,9 @@ submitButton.addEventListener('click', function (e) {
     var countryName = getSelectedOption(countryList);
     const month = dateSelect.value;
     var chosenDate = "confirmed?from=" + month + "-01T00:00:00Z&to=" + month + "-30T00:00:00Z"
+    // Change the calendar title
+    yearMonth = month.split("-")
+    calendarTitle.innerHTML = months[yearMonth[1]] + " " + yearMonth[0]
 
     fetch("https://api.covid19api.com/country/" + countryName.value + "/status/" + chosenDate, requestOptions)
         .then(response => response.json())
